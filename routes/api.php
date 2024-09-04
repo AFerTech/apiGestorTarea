c176071a615a26eaf2365e3f7a4d13063892f7c3
Alexis Fernando <alexis96fer@gmail.com>
2024-09-04 13:16:11 -0600

    add apiRoute para definir rutas con apiResource

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CategoryController;
+
+Route::get('/', function (Request $request) {
+    return 'desde api';
+})->middleware('auth:sanctum');
+
+
+Route::apiResource('tasks', TaskController::class);
+Route::apiResource('categories', CategoryController::class);
